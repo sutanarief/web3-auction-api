ac8ce724a5f905eb4cb28df3a4a9d33ba48e9269
sutanarief <sutan1412@gmail.com>
2024-01-13 01:20:32 +0700

    add post git

--- app.js
+++ app.js
@@ -8,10 +8,38 @@ const PORT = 3000
 const router = require('./router')
 
 const app = express()
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const { execSync } = require('child_process');
 
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }))
+
+app.post('/git', (req, res) => {
+  const hmac = crypto.createHmac('sha1', process.env.SECRET);
+  const sig  = 'sha1=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+  if (req.headers['x-github-event'] === 'push' &&
+    crypto.timingSafeEqual(Buffer.from(sig), Buffer.from(req.headers['x-hub-signature']))) {
+    res.sendStatus(200);
+    const commands = ['git fetch origin master',
+                      'git reset --hard origin/master',
+                      'git pull origin master --force',
+                      'npm install',
+                      // your build commands here
+                      'refresh']; // fixes glitch ui
+    for (const cmd of commands) {
+      console.log(execSync(cmd).toString());
+    }
+    console.log('updated with origin/master!');
+    return;
+  } else {
+    console.log('webhook signature incorrect!');
+    return res.sendStatus(403);
+  }
+});
+
 app.use('/api', router)
 //yuhuuuu
 
